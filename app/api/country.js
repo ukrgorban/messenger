a7579c773bd42868d880ef1d58f568d0f0c8dd11
“Gorban <ukrgorban@gmail.com>
2024-09-25 12:10:44 +0300

    vm

--- app/api/country.js
+++ app/api/country.js
@@ -1,5 +1,13 @@
-'use strict';
+const country = db('country');
 
-const db = require('../db.js');
+({
+    async read(id) {
+        console.log({ db });
+        return await country.read(id);
+    },
 
-module.exports = db('country');
+    async find(mask) {
+        const sql = 'SELECT * from country where name like $1';
+        return await country.query(sql, [mask]);
+    },
+});
--- app/api/user.js
+++ app/api/user.js
@@ -1,26 +1,24 @@
-'use strict';
-
-const db = require('../db.js');
-const hash = require('../hash.js');
-
-const users = db('users');
-
-module.exports = {
+({
     async read(id) {
-        return await users.read(id, ['id', 'login']);
+        return await db('users').read(id, ['id', 'login']);
     },
 
     async create({ login, password }) {
-        const passwordHash = await hash(password);
-        return await users.create({ login, password: passwordHash });
+        const passwordHash = await common.hash(password);
+        return await db('users').create({ login, password: passwordHash });
     },
 
     async update(id, { login, password }) {
-        const passwordHash = await hash(password);
-        return await users.update(id, { login, password: passwordHash });
+        const passwordHash = await common.hash(password);
+        return await db('users').update(id, { login, password: passwordHash });
     },
 
     async delete(id) {
-        return await users.delete(id);
+        return await db('users').delete(id);
     },
-};
+
+    async find(mask) {
+        const sql = 'SELECT login from users where login like $1';
+        return await db('users').query(sql, [mask]);
+    },
+});
